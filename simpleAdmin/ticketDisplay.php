df1d76e2189a63da8b4d076af83b5cebcf2f6d3e
miguelf98 <miguel.fernandes123@outlook.pt>
2017-04-19 13:10:31 +0100

    asd123

--- simpleAdmin/ticketDisplay.php
+++ simpleAdmin/ticketDisplay.php
@@ -3,84 +3,103 @@
         <link rel="stylesheet" href="assets/css/ticketStylesheet.css" type="text/css">
     </head>
     <style>
+        body{
+            font-family: "Roboto", helvetica, arial, sans-serif;
+        }
+
         #ticketContainer{
             width: 100%;
+            height: 100%;
             min-height: 15%;
         }
 
-        .ticketRow{
-            position: relative;
-            width: 99.3%;
-            height: 250px;
-            border: 5px solid black;
-            margin-bottom: 10px;
-        }
 
         /* TICKET NUMBER CONTAINER **********************/
 
         .ticketNumberContainer{
-            width: 22%;
-            height: 93.5%;
             position: relative;
-            margin: 5px;
             text-align: center;
             vertical-align: middle;
-            float: left;
-            border-right: 3px solid black;
-
-        }
-
-        .ticketNumberContainer p{
+            height: 180px;
             font-size: 150px;
-            position: relative;
-            bottom: 150px;
-            color: #64dd17;
+            display: flex;
         }
 
-        .ticketNumberContainer span{
-            font-size: 50px;
-
+        .floatLeft{
+            float: left;
         }
 
-
-        /* TICKET INFO CONTAINER *****************/
-
-        .ticketInfoContainer{
-            display: inline-flex;
-            position: relative;
-            height: 93.5%;
-            width: 75.3%;
-            margin: 5px;
-            border: 1px solid black;
-        }
-
-        .ticketInfoContainer .produtoNome{
-            font-size: 100px;
+        .floatRight{
             float: right;
         }
 
+        #titleContainer{
+            font-size: 50px;
+            margin-left: 25px;
+            height:100px;
+
+        }
+
+        #title1{
+            float: left;
+            margin-left: 15px;
+
+        }
+
+        #title2{
+            float: right;
+            margin-right: 275px;
+        }
+
+
+
+        #prepararTicketContainer{
+            width: 300px;
+            margin-left: 15px;
+            float: left;
+            color: #dd3d50;
+
+        }
+
+        #entregarTicketContainer{
+            float: right;
+            margin-right: 250px;
+            color: #64dd17;
+
+        }
+
+        .divider{
+            position:absolute;
+            left:50%;
+            top:10%;
+            bottom:10%;
+            border-left:3px
+        }
 
     </style>
 
 <body>
-    <div id="ticketContainer">
-        <div class="ticketRow">
-
-            <!-- TODO: DB TABLE SAVE TICKET INFO (userId,ticket#,order info) -->
-            <!-- TODO: ASSIGN TICKET ORDER IN PILE AND DISPLAY IT WHEN BEING PREPARED -->
-
-            <div class="ticketNumberContainer">
-                <span># TICKET</span>
-                <p>A10</p>
-            </div>
-
-            <div class="ticketInfoContainer">
-                <span class="produtoNome">Tosta MISTAAAAAAAAAAAAAa</span>
-            </div>
+    <div id="titleContainer">
+        <div id="title1">A preparar</div>
+        <div id="title2">A entregar</div>
+    </div>
+    <div id="ticketContainer" >
+        <div id="prepararTicketContainer">
+                <div class="ticketNumberContainer">Z95</div>
+                <div class="ticketNumberContainer">Z96</div>
+                <div class="ticketNumberContainer">Z97</div>
+                <div class="ticketNumberContainer">Z98</div>
         </div>
-        <div class="ticketRow"></div>
-        <div class="ticketRow"></div>
-        <div class="ticketRow"></div>
+
+        <div class="divider"></div>
+
+        <div id="entregarTicketContainer">
+            <div class="ticketNumberContainer ">Z95</div>
+            <div class="ticketNumberContainer ">Z96</div>
+            <div class="ticketNumberContainer ">Z97</div>
+            <div class="ticketNumberContainer">Z98</div>
+        </div>
+
     </div>
 </body>
 </html>
